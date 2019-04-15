f5c7f754437f768f1eacd07722cf1b14d28827da
yht <930237972@qq.com>
2019-04-15 18:32:52 +0800

    1

---
+++ src/main/java/com/yht/demo/XindaitongServerApplication.java
@@ -0,0 +1,13 @@
+package com.yht.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XindaitongServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XindaitongServerApplication.class, args);
+    }
+
+}
