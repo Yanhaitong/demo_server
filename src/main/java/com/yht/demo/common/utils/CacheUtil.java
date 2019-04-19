feedecd5a43e6bd91a99b37544f869c57cacb058
930237972@qq.com <930237972@qq.com>
2019-04-19 16:04:09 +0800

    登录接口

--- src/main/java/com/yht/demo/common/utils/CacheUtil.java
+++ src/main/java/com/yht/demo/common/utils/CacheUtil.java
@@ -5,15 +5,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
-import javax.xml.ws.Action;
 import java.util.concurrent.TimeUnit;
 
 public class CacheUtil {
 
     private static final Logger log = LoggerFactory.getLogger(CacheUtil.class);
-
-    @Autowired
-    private StringRedisTemplate stringRedisTemplate;
 
     /**
      * 存放图片验证码
@@ -22,6 +18,7 @@ public class CacheUtil {
      */
     public void saveCaptcha(String key) {
         log.info("调用saveCaptcha==========" + key);
+        StringRedisTemplate stringRedisTemplate = new StringRedisTemplate();
         stringRedisTemplate.delete(key);
         stringRedisTemplate.expire(key, 60 * 30, TimeUnit.SECONDS);
     }
